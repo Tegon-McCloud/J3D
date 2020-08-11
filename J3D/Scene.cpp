61dd0a4b0b348c975dfeecf9466003d83978dfbe
Tegon-McClouds <Tegon.mccloud@gmail.com>
2020-08-11 10:52:05 +0200

    hehe

--- J3D/Scene.cpp
+++ J3D/Scene.cpp
@@ -1,3 +1,40 @@
 #include "Scene.h"
 
+SceneNode::SceneNode() : pParent(nullptr) {}
+
+void SceneNode::move(SceneNode* pNewParent) {
+	if (pParent) {
+		auto it = std::find(pParent->children.begin(), pParent->children.end(), this);
+		if (it != pParent->children.end()) {
+			pParent->children.erase(it);
+		}
+	}
+}
+
 Scene::Scene() {}
+
+SceneNode* Scene::getNode(const std::string& name) {
+	auto it = nodeNames.find(name);
+	if (it == nodeNames.end()) {
+		return nullptr;
+	}
+	return it->second;
+}
+
+SceneNode* Scene::getNode(size_t index) {
+	return &nodes[index];
+}
+
+Mesh* Scene::getMesh(const std::string& name) {
+	auto it = meshNames.find(name);
+	if (it == meshNames.end()) {
+		return nullptr;
+	}
+	return it->second;
+}
+
+Mesh* Scene::getMesh(size_t index) {
+	return &meshes[index];
+}
+
+
--- J3D/Scene.h
+++ J3D/Scene.h
@@ -1,24 +1,40 @@
 #pragma once
 
+#include "Mesh.h"
+
 #include <vector>
+#include <unordered_map>
 
 class SceneNode {
 public:
-	SceneNode() = default;
+	SceneNode();
 
+
+	void move(SceneNode* pNewParent);
 
 private:
 	SceneNode* pParent;
 	std::vector<SceneNode*> children;
-
+	std::vector<Mesh*> meshes;
 };
 
 class Scene {
 public:
 	Scene();
 
+	SceneNode* getNode(const std::string& name);
+	SceneNode* getNode(size_t index);
+	Mesh* getMesh(const std::string& name);
+	Mesh* getMesh(size_t index);
+
 private:
 	SceneNode root;
-	
+	std::vector<SceneNode> nodes;
+	std::vector<Mesh> meshes;
+
+	std::unordered_map<std::string, SceneNode*> nodeNames;
+	std::unordered_map<std::string, Mesh*> meshNames;
+
+
 };
 
